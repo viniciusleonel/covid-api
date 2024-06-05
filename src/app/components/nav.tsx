f742682cbb6bb0da076302042a75bed4b7ddac89
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-06-05 15:37:17 -0300

    feat: Inserindo imagem de profile, links do github e linkedin

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Logo from "../../../images/prosesmt_logo.jpeg"
+import Logo from "../../../images/vinicius.jpeg"
 import { InputHTMLAttributes, ReactElement } from "react"
 import Link from "next/link";
 
@@ -14,18 +14,27 @@ export default function Nav({input} : NavProps) {
         <nav className="fixed top-0 left-0 w-full z-50 px-3 py-1 md:py-3 border-b-2 dark:border-b-2 border-cyan-700 bg-white">
         <div className="mx-3 flex items-center justify-between">
             <div className="flex items-center justify-center gap-4">
-                <div className="w-[25px] h-[25px] md:w-[40px] md:h-[40px] lg:w-[50px] lg:h-[50px]">
-                <Image 
-                    src={Logo}
-                    alt="ProSESMT l=Logo"
-                />
-                </div>
+                <div className="w-[25px] h-[25px] md:w-[40px] md:h-[40px] lg:w-[50px] lg:h-[50px] ">
                 <Link 
-                    className=" text-lg md:text-xl lg:text-2xl font-bold"
-                    href={'https://prosesmt.com.br/site/'}
+                    className=" text-lg md:text-xl lg:text-2xl font-bold hover:text-cyan-700"
+                    href={'https://www.linkedin.com/in/viniciuslps/'}
                     target="_blank"
                     rel="noopener noreferrer"
-                >ProSESMT</Link>
+                    ><Image 
+                        className="rounded-full border-2 border-black"
+                        src={Logo}
+                        alt="Vinicius Profile Picture"
+                    />
+                </Link>
+                
+                </div>
+                <Link 
+                    className=" text-lg md:text-xl lg:text-2xl font-bold hover:text-cyan-700"
+                    href={'https://github.com/viniciusleonel/covid-api'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Vinicius Leonel
+                </Link>
             </div>
 
             <div>
